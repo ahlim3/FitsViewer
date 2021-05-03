2bba5a46608a9776628e829103c16e3638981021
ahlim3 <78980758+ahlim3@users.noreply.github.com>
2021-05-03 00:57:32 -0500

    DDP with multiple Window View

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -13,113 +13,245 @@ import Accelerate.vImage
 import Combine
 import CoreGraphics
 
-struct ContentView: View {
-
-    
-
-    var path = "file:///Users/anthonylim/Downloads/2020-12-03_19;56;17.fits"
-    let path1 = "file:///Users/anthonylim/Downloads/2020-12-03_19;56;17.fits"
-    let path2 = "file:///Users/anthonylim/Downloads/n5194.fits"
-    let path3 = "file:///Users/anthonylim/Downloads/HIP115691-ID14333-OC148763-GR7975-LUM.fit"
-    let path4 = "file:///Users/anthonylim/Downloads/JtIMAGE_009.fits"
-    let path5 = "file:///Users/anthonylim/Downloads/2020-12-03_19_16_43.fits"
-    //Work
-    let path6 = "file:///Users/anthonylim/Downloads/moon_BIN_1x1_0.0010s_002.fits"
-    
-    let path7 = "file:///Users/anthonylim/Downloads/NGC4438-104275-LUM.fit"
-    //Does not work with the black point on outside
-    let path8 = "file:///Users/anthonylim/Downloads/M66-ID10979-OC144423-GR4135-LUM2.fit"
-    // Work
-    let path9 = "file:///Users/anthonylim/Downloads/NGC6960-ID14567-OC148925-GR8123-LUM.fit"
-    //work
-    let path10 = "file:///Users/anthonylim/Downloads/globular.fits"
-    let path11 = "file:///Users/anthonylim/Downloads/C2019Y4-ID10934-OC144374-GR4095-LUM2.fit"
-    let histogramcount = 1024
-    @State var called = 0
-    
-    func read() -> ([FITSByte_F],vImage_Buffer,vImage_CGImageFormat){
-        var threeData: ([FITSByte_F],vImage_Buffer,vImage_CGImageFormat)?
-        var path = URL(string: path6)!
-        var read_data = try! FitsFile.read(contentsOf: path)
-        let prime = read_data?.prime
-        print(prime)
-        prime?.v_complete(onError: {_ in
-            print("CGImage creation error")
-        }) { result in
-            threeData = result
-        }
-        return threeData!
-    }
-
-    
-    func display() -> ([vImagePixelCount], CGImage, CGImage, CGImage){
-        let threedata = read()
-        //target data
-        var data = threedata.0
-        //Buffer from FITS File
-        var buffer = threedata.1
-        //Grayscale format from FITS file
-        let format = threedata.2
-        //destination buffer
-        var buffer2 = buffer
-        var buffer4 = buffer
-        let dataMaxPixel = Pixel_F(data.max()!)
-        let dataMinPixel = Pixel_F(data.min()!)
-        var histogramBin = histogram(dataMaxPixel: dataMaxPixel, dataMinPixel: dataMinPixel, buffer: buffer, histogramcount: histogramcount)
-        //Return three data, Histogram(0), Maximum Pixel Value(1), Minimum Pixel Value(2), Cutoff?(3) = 0 no, 1 yes
-        var OptimizedHistogramContents = optimizedHist(histogram_in: histogramBin, histogramcount: histogramcount)
-        let lowerPixelLimit = OptimizedHistogramContents.2
-        let upperPixelLimit = OptimizedHistogramContents.1
-        let cutoff = OptimizedHistogramContents.3
-        var histogramOpt = histogram(dataMaxPixel: upperPixelLimit, dataMinPixel: lowerPixelLimit, buffer: buffer, histogramcount: histogramcount)
-        print(histogramOpt)
-        var buffer3 = buffer
-        let kernelwidth = 7
-        let kernelheight = 7
-        var A : Float = 1.0
-        var simgaX: Float = 0.75
-        var sigmaY: Float = 0.75
-        
-        var kernelArray = kArray(width: kernelwidth, height: kernelheight, sigmaX: simgaX, sigmaY: sigmaY, A: A)
-        
-        print(kernelArray, " " , kernelArray.max())
-        vImageConvolve_PlanarF(&buffer, &buffer3, nil, 0, 0, &kernelArray, UInt32(kernelwidth), UInt32(kernelheight), 0, vImage_Flags(kvImageEdgeExtend))
-
-        var BlurredPixelData = (buffer3.data.toArray(to: Float.self, capacity: Int(buffer3.width*buffer3.height)))
-        var OriginalPixelData = (buffer.data.toArray(to: Float.self, capacity: Int(buffer.width*buffer.height)))
-        var bendvalue = bendValue(AdjustedData: BlurredPixelData, lowerPixelLimit: lowerPixelLimit) //return bendvalue as .0, and averagepixeldata as .1
-        var ddpPixelData = ddpProcessed(OriginalPixelData: OriginalPixelData, BlurredPixeldata: BlurredPixelData, Bendvalue: bendvalue.0, AveragePixel: bendvalue.1, cutOff: cutoff, MinPixel: lowerPixelLimit)
-        let ddpScaled = ddpScaled(ddpPixelData: ddpPixelData, MinPixel: lowerPixelLimit)
-        
-        let DDPwithScale = returningCGImage(data: ddpScaled, buffer: buffer3)
-        let originalImage = (try? buffer.createCGImage(format: format))!
-        let DDPwithoutScale = returningCGImage(data: ddpPixelData, buffer: buffer3)
-        
-        print("called")
-        
-        return(histogramBin, originalImage, DDPwithoutScale, DDPwithScale)
-    }
-
+struct DestinationPageView: View {
+    let path = "file:///Users/anthonylim/Downloads/C2019Y4-ID10934-OC144374-GR4095-LUM2.fit"
     var body: some View {
-        let modified = display()
-
-                    TabView{
-                        HStack{
-                            Image(decorative: modified.1, scale: 1.0)
-                                .resizable()
-                                .scaledToFit()
+        let modified = display(Path: path)
+        TabView{
+            HStack{
+        Image(decorative: modified.1, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+            }
+        HStack{
+        Image(decorative: modified.2, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+        }
+    }
+}
+}
+struct DestinationPageView1: View {
+     let path1 = "file:///Users/anthonylim/Downloads/2020-12-03_19;56;17.fits"
+    var body: some View {
+        let modified = display(Path: path1)
+        TabView{
+            HStack{
+        Image(decorative: modified.1, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+            }
+        HStack{
+        Image(decorative: modified.2, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+        }
+    }
+}
+}
+struct DestinationPageView2: View {
+    let path2 = "file:///Users/anthonylim/Downloads/n5194.fits"
+    var body: some View {
+        let modified = display(Path: path2)
+        TabView{
+            HStack{
+        Image(decorative: modified.1, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+            }
+        HStack{
+        Image(decorative: modified.2, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+        }
+    }
+}
+}
+struct DestinationPageView3: View {
+    let path10 = "file:///Users/anthonylim/Downloads/globular.fits"
+    var body: some View {
+        let modified = display(Path: path10)
+        TabView{
+            HStack{
+        Image(decorative: modified.1, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+            }
+        HStack{
+        Image(decorative: modified.2, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+        }
+    }
+}
+}
+struct DestinationPageView4: View {
+     let path4 = "file:///Users/anthonylim/Downloads/JtIMAGE_009.fits"
+    var body: some View {
+        let modified = display(Path: path4)
+        TabView{
+            HStack{
+        Image(decorative: modified.1, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+            }
+        HStack{
+        Image(decorative: modified.2, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+        }
+    }
+}
+}
+struct DestinationPageView5: View {
+     let path5 = "file:///Users/anthonylim/Downloads/2020-12-03_19_16_43.fits"
+    var body: some View {
+        let modified = display(Path: path5)
+        TabView{
+            HStack{
+        Image(decorative: modified.1, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+            }
+        HStack{
+        Image(decorative: modified.2, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+        }
+    }
+}
+}
+struct DestinationPageView6: View {
+     let path6 = "file:///Users/anthonylim/Downloads/moon_BIN_1x1_0.0010s_002.fits"
+    var body: some View {
+        let modified = display(Path: path6)
+        TabView{
+            HStack{
+        Image(decorative: modified.1, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+            }
+        HStack{
+        Image(decorative: modified.2, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+        }
+    }
+}
+}
+struct DestinationPageView7: View {
+     let path7 = "file:///Users/anthonylim/Downloads/NGC4438-104275-LUM.fit"
+    var body: some View {
+        let modified = display(Path: path7)
+        TabView{
+            HStack{
+        Image(decorative: modified.1, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+            }
+        HStack{
+        Image(decorative: modified.2, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+        }
+    }
+}
+}
+struct DestinationPageView8: View {
+     let path8 = "file:///Users/anthonylim/Downloads/M66-ID10979-OC144423-GR4135-LUM2.fit"
+     // Work
+    var body: some View {
+        let modified = display(Path: path8)
+        TabView{
+            HStack{
+        Image(decorative: modified.1, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+            }
+        HStack{
+        Image(decorative: modified.2, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+        }
+    }
+}
+}
+struct DestinationPageView9: View {
+     let path9 = "file:///Users/anthonylim/Downloads/NGC6960-ID14567-OC148925-GR8123-LUM.fit"
+    var body: some View {
+        let modified = display(Path: path9)
+        TabView{
+            HStack{
+        Image(decorative: modified.1, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+            }
+        HStack{
+        Image(decorative: modified.2, scale: 1.0)
+            .resizable()
+            .scaledToFit()
+        }
+    }
+}
+}
+struct ContentView: View {
+    var body: some View {
+                NavigationView {
+                    List {
+                        NavigationLink(
+                            destination: DestinationPageView()
+                        ) {
+                            Text("Image 1")
                         }
-                        HStack{
-                            Image(decorative: modified.2, scale: 1.0)
-                                .resizable()
-                                .scaledToFit()
-
+                        NavigationLink(
+                            destination: DestinationPageView1()
+                        ) {
+                            Text("Image 2")
                         }
-                        HStack{
-                            Image(decorative: modified.3, scale: 1.0)
-                                .resizable()
-                                .scaledToFit()
+                        NavigationLink(
+                            destination: DestinationPageView2()
+                        ) {
+                            Text("Image 3")
                         }
+                        NavigationLink(
+                            destination: DestinationPageView3()
+                        ) {
+                            Text("Image 4")
+                        }
+                        NavigationLink(
+                            destination: DestinationPageView4()
+                        ) {
+                            Text("Image 5")
+                        }
+                        NavigationLink(
+                            destination: DestinationPageView5()
+                        ) {
+                            Text("Image 6")
+                        }
+                        NavigationLink(
+                            destination: DestinationPageView6()
+                        ) {
+                            Text("Image 7")
+                        }
+                        NavigationLink(
+                            destination: DestinationPageView7()
+                        ) {
+                            Text("Image 8")
+                        }
+                        NavigationLink(
+                            destination: DestinationPageView8()
+                        ) {
+                            Text("Image 9")
+                        }
+                        NavigationLink(
+                            destination: DestinationPageView9()
+                        ) {
+                            Text("Image 10")
+                        }
+                        
+                    }
+                    Text("Select a Image page from the links.")
                 }
-}
-}
+            }
+        }
+
--- Shared/Extensions.swift
+++ Shared/Extensions.swift
@@ -94,6 +94,7 @@ func ddpProcessed(OriginalPixelData: [Float], BlurredPixeldata: [Float], Bendval
         ddpPixeldata[i] = AveragePixel * ((OriginalPixelData[i]/(BlurredPixeldata[i] + Bendvalue)))
         }
     var temp = (ddpPixeldata.max(), ddpPixeldata.min())
+    print(temp)
     return ddpPixeldata
 }
 func ddpScaled(ddpPixelData: [Float], MinPixel : Pixel_F) -> [Float]{
@@ -175,4 +176,77 @@ func optimizedHist(histogram_in : [vImagePixelCount], histogramcount : Int) -> (
     var MaxPixel_F = Pixel_F(Float(MaxPixel) / Float(histogramcount))
     var MinPixel_F = Pixel_F(Float(MinPixel) / Float(histogramcount))
     return (optimizedHist, MaxPixel_F, MinPixel_F, minimumCutoff)
+}
+func forcingMeanData(PixelData : [Float], MinimumLimit: Float) -> [Float]{
+    var PixelData = PixelData
+    for i in 0 ..< PixelData.count{
+        if PixelData[i] < MinimumLimit{
+            PixelData[i] = MinimumLimit
+        }
+    }
+    return PixelData
+}
+func read(Path: String) -> ([FITSByte_F],vImage_Buffer,vImage_CGImageFormat){
+    var threeData: ([FITSByte_F],vImage_Buffer,vImage_CGImageFormat)?
+    var path = URL(string: Path)!
+    var read_data = try! FitsFile.read(contentsOf: path)
+    let prime = read_data?.prime
+    print(prime)
+    prime?.v_complete(onError: {_ in
+        print("CGImage creation error")
+    }) { result in
+        threeData = result
+    }
+    return threeData!
+}
+
+
+func display(Path: String) -> ([vImagePixelCount], CGImage, CGImage){
+    let threedata = read(Path: Path)
+    //target data
+    var data = threedata.0
+    //Buffer from FITS File
+    var buffer = threedata.1
+    //Grayscale format from FITS file
+    let format = threedata.2
+    //destination buffer
+    var buffer2 = buffer
+    var buffer4 = buffer
+    let histogramcount = 1024
+    let dataMaxPixel = Pixel_F(data.max()!)
+    let dataMinPixel = Pixel_F(data.min()!)
+    var histogramBin = histogram(dataMaxPixel: dataMaxPixel, dataMinPixel: dataMinPixel, buffer: buffer, histogramcount: histogramcount)
+    //Return three data, Histogram(0), Maximum Pixel Value(1), Minimum Pixel Value(2), Cutoff?(3) = 0 no, 1 yes
+    var OptimizedHistogramContents = optimizedHist(histogram_in: histogramBin, histogramcount: histogramcount)
+    let lowerPixelLimit = OptimizedHistogramContents.2
+    let upperPixelLimit = OptimizedHistogramContents.1
+    let cutoff = OptimizedHistogramContents.3
+    var histogramOpt = histogram(dataMaxPixel: upperPixelLimit, dataMinPixel: lowerPixelLimit, buffer: buffer, histogramcount: histogramcount)
+    print(histogramOpt)
+    var buffer3 = buffer
+    let kernelwidth = 7
+    let kernelheight = 7
+    var A : Float = 1.0
+    var simgaX: Float = 0.75
+    var sigmaY: Float = 0.75
+    
+    var kernelArray = kArray(width: kernelwidth, height: kernelheight, sigmaX: simgaX, sigmaY: sigmaY, A: A)
+    
+    print(kernelArray, " " , kernelArray.max())
+    vImageConvolve_PlanarF(&buffer, &buffer3, nil, 0, 0, &kernelArray, UInt32(kernelwidth), UInt32(kernelheight), 0, vImage_Flags(kvImageEdgeExtend))
+
+    var BlurredPixelData = (buffer3.data.toArray(to: Float.self, capacity: Int(buffer3.width*buffer3.height)))
+    var bendvalue = bendValue(AdjustedData: BlurredPixelData, lowerPixelLimit: lowerPixelLimit) //return bendvalue as .0, and averagepixeldata as .1
+    var OriginalPixelData = (buffer.data.toArray(to: Float.self, capacity: Int(buffer.width*buffer.height)))
+    //OriginalPixelData = forcingMeanData(PixelData: OriginalPixelData, MinimumLimit: lowerPixelLimit)
+    var ddpPixelData = ddpProcessed(OriginalPixelData: OriginalPixelData, BlurredPixeldata: BlurredPixelData, Bendvalue: bendvalue.0, AveragePixel: bendvalue.1, cutOff: cutoff, MinPixel: lowerPixelLimit)
+    let ddpScaled = ddpScaled(ddpPixelData: ddpPixelData, MinPixel: lowerPixelLimit)
+    
+    let DDPwithScale = returningCGImage(data: ddpScaled, buffer: buffer3)
+    let originalImage = (try? buffer.createCGImage(format: format))!
+    let DDPwithoutScale = returningCGImage(data: ddpPixelData, buffer: buffer3)
+    
+    print("called")
+    
+    return(histogramBin, originalImage,  DDPwithScale)
 }
